15290084edb5c8c9740ceff9d9f7edcebc677554
andrelreis <reisandreluis@gmail.com>
2024-08-23 13:02:48 -0300

    Improving the importing of functions, modules and classes

---
+++ archetype/__init__.py
@@ -0,0 +1,5 @@
+# Import functions and classes
+
+from .functions_0 import product
+from .modules_1 import functions_1
+from .modules_1.functions_1 import add_one
---
+++ archetype/modules_1/__init__.py
@@ -0,0 +1,1 @@
+from .functions_1 import add_one
